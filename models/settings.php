74254401326ec079ab39d6c99312fa298ef6a688
driedfruit <driedfruit@mindloop.net>
2015-09-29 21:27:38 +0400

    Correctly indent Settings model.

--- models/settings.php
+++ models/settings.php
@@ -7,41 +7,41 @@ class Setting extends ORM_Model {
 	public $value;
 	public $hints;
 
-    static $_sql = array(
-        'id' => 'MEDIUMINT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY',
-        'name' => 'VARCHAR(255) NOT NULL UNIQUE',
-        'value' => 'TEXT NOT NULL',
-        'hints' => 'VARCHAR(255) NOT NULL',
-    );
+	static $_sql = array(
+		'id' => 'MEDIUMINT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY',
+		'name' => 'VARCHAR(255) NOT NULL UNIQUE',
+		'value' => 'TEXT NOT NULL',
+		'hints' => 'VARCHAR(255) NOT NULL',
+	);
 
-    static function Get($item = null, $default = null, $raw = false) {
-    	static $config = null;
-    	static $settings = null;
-    	if ($config == null) {
-    		$settings = ORM::Collection('Setting');
-    		$config = array();	
-    		foreach ($settings as $setting) {
-    			$config[$setting->name] = $setting->value;
-    		}
-    	}
-    	if ($raw === true) {
-    		return $settings;
-    	}
-    	if ($item == null) {
-    		return $config;	
-    	}
-    	if (isset($config[$item])) {
-    		return $config[$item];
-    	} else if ($default) {
-    		$sett = new Setting();
-    		$sett->name = $item;
-    		$sett->value = $default;
+	static function Get($item = null, $default = null, $raw = false) {
+		static $config = null;
+		static $settings = null;
+		if ($config == null) {
+			$settings = ORM::Collection('Setting');
+			$config = array();
+			foreach ($settings as $setting) {
+				$config[$setting->name] = $setting->value;
+			}
+		}
+		if ($raw === true) {
+			return $settings;
+		}
+		if ($item == null) {
+			return $config;
+		}
+		if (isset($config[$item])) {
+			return $config[$item];
+		} else if ($default) {
+			$sett = new Setting();
+			$sett->name = $item;
+			$sett->value = $default;
 			$sett->hints = '';
 			$sett->save();
 			return $default;
-    	}
-    	return null;
-    }
+		}
+		return null;
+	}
 }
 
 ?>
